b3821bcf3dd389ea4f23acad404e1f1dc26c632a
Oh-Junho-KR <81357906+Oh-Junho-KR@users.noreply.github.com>
2024-10-28 14:18:59 +0900

    [소수 만들기] Summer/Winter Coding(~2018)

--- JavaScript/Lv1/MakingPrime.js
+++ JavaScript/Lv1/MakingPrime.js
@@ -1,10 +1,11 @@
 var solution = function (nums) {
-    var answer = 0;
+    var answer = 0; // 결과값
     
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             for (let k = j + 1; k < nums.length; k++) {
                 let sum = nums[i] + nums[j] + nums[k];
+                // 소수 판별
                 if (fnIsPrime(sum)) {
                     answer++;
                 }
